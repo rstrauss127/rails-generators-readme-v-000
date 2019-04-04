85f1a5a41cca6f13c7ecd9112b061433d13e0b23
Rebecca Strauss <rstrauss127@gmail.com>
2019-04-04 22:27:33 +0000

    Automatically backed up by Learn

--- db/migrate/20190404222423_change_post_status_data_type_to_posts.rb
+++ db/migrate/20190404222423_change_post_status_data_type_to_posts.rb
@@ -1,5 +1,5 @@
 class ChangePostStatusDataTypeToPosts < ActiveRecord::Migration[5.0]
   def change
-    add_column :post_status, :published_status, :string
+    add_column :posts, :post_status, :string
   end
 end
